96bced80a7c2346010db5ce8d6079ae2c4f32b0d
josetapadas <jose.alves@canonical.com>
2026-03-05 17:48:37 +0000

    intial simple module

---
+++ hello_kernel.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jose Tapadas Alves");
+MODULE_DESCRIPTION("My first kernel module");
+
+static int __init hello_init(void)
+{
+    pr_info("[-] hello_kernel: module loaded!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    pr_info("[-] hello_kernel: module unloaded!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
